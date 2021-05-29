1139593af20822ec5450dea0fd895cef1226b28a
Herba <Amberli01bolden@gmail.com>
2021-05-29 17:35:19 +0800

    0.5.0 Update
    Created on May 29, 2021.
    (Update 1 of 2)
    - Could be the final update to our project before declaring 1.0
    - 95% complete

--- script.js
+++ script.js
@@ -4,3 +4,43 @@ Was trying to implement animations on the index.html page.
 could not maximize the productivity for today.
 I will try adding JS once I am done with the foundations.
 */
+
+function togglePopup() {
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function togglePopup2() {
+    document.getElementById("popup-2").classList.toggle("active");
+}
+
+function togglePopup3() {
+    document.getElementById("popup-3").classList.toggle("active");
+}
+
+function togglePopup4() {
+    document.getElementById("popup-4").classList.toggle("active");
+}
+
+function togglePopup5() {
+    document.getElementById("popup-5").classList.toggle("active");
+}
+
+function togglePopup6() {
+    document.getElementById("popup-6").classList.toggle("active");
+}
+
+function togglePopup7() {
+    document.getElementById("popup-7").classList.toggle("active");
+}
+
+function togglePopup8() {
+    document.getElementById("popup-8").classList.toggle("active");
+}
+
+function togglePopup9() {
+    document.getElementById("popup-9").classList.toggle("active");
+}
+
+function togglePopup10() {
+    document.getElementById("popup-10").classList.toggle("active");
+}
